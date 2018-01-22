f238f62a47c21e60882c7a602913c219d04993f4
Sumit <sumit.kumar@eurecom.fr>
2018-01-22 22:10:32 +0100

    another unstable intermediate commit

--- lib/equalizer/ls_soft.cc
+++ lib/equalizer/ls_soft.cc
@@ -23,90 +23,88 @@ using namespace gr::ieee802_11::equalizer_soft;
 
 void ls_soft::equalize_soft(gr_complex *in, int n, gr_complex *symbols, uint8_t *bits, float *llr, boost::shared_ptr<gr::digital::constellation> mod_soft, int d_frame_symbols) {
 	
-	if(n == 0) {
-std::cout << "n->" << n <<" interference->" << d_interference << " d_frame_symbols->" << d_frame_symbols << std::endl;
+	if(n == 0) 
+	{
+//std::cout << "n->" << n <<" interference->" << d_interference << " d_frame_symbols->" << d_frame_symbols << std::endl;
 		std::memcpy(d_H_soft, in, 64 * sizeof(gr_complex)); // first lts copied in d_H
 
-	} else if(n == 1) { // the second lts now 
+	} 
+	else if(n == 1) 
+	{ // the second lts now 
 
-                //imt = 1; // check point var, set in one section, to be accessed in another section during multiple calls of equalize_soft
+//imt = 1; // check point var, set in one section, to be accessed in another section during multiple calls of equalize_soft
                 double signal = 0;
 		double noise = 0;
 		int start = 5; // start sub carrier of interference
 		int stop = start+11; // stop subcarrier of interference
-		int noise_interf = 0; // noise variance of interfered band
-		int noise_non_interf = 0; // noise variance of non-interfered band
+		int noise_interf = 0; // local noise variance of interfered band
+		int noise_non_interf = 0; // local noise variance of non-interfered band
 		int conv_est = 0; // noise variance conv method
 		int temp1 = 0;
 		int temp2 = 0;
-	
-		for(int i = 0; i < 64; i++) 
+		int temp3 = 0;
+		int temp4 = 0;
+// calculation loop	
+		for(int i = 0; i < 64; i++) // loop noise vars calc
 		{ 
-		// skip null padded subs and the dc 		
-		if((i == 32) || (i < 6) || ( i > 58)) {continue;}
-
-                d_N_soft_indv[i] = (std::pow(std::abs(d_H_soft[i] - in[i]), 2))/2;// always do it
-
-                if(d_N_soft_indv[i] > 100) 
-		{d_interference = 1;
-std::cout << "n->" << n <<" interference->" << d_interference << " d_frame_symbols->" << d_frame_symbols << std::endl;
-		}
-		// local noise variance estimate : when interference starts
-		if(d_interference)
-		{
+			if((i == 32) || (i < 6) || ( i > 58)) 
+			{
+				continue; // skip nulls and dc
+			} 
+			if(i >= 6 && i <= 16) // TODO check these indices 
+			{       // sum over interfered subs
+				temp3  += (std::pow(std::abs(d_H_soft[i] - in[i]), 2))/2;
+			}
+			else
+			{       // sum over non interferred subs
+				temp4 += (std::pow(std::abs(d_H_soft[i] - in[i]), 2))/2;
+			} 
 			if(i > start && i <= stop)
 			{
-			//std::cout << "interference band" << std::endl;
-                        //std::cout << i << std::endl;
-			temp1 = (std::pow(std::abs(d_H_soft[i] - in[i]), 2))/(2*(stop-start+1));
-			noise_interf += temp1;
+			noise_interf += (std::pow(std::abs(d_H_soft[i] - in[i]), 2));
 			}
 			else
 			{
-			//std::cout << "non interference" << std::endl;
-                        //std::cout << i << std::endl;
-			temp2 = (std::pow(std::abs(d_H_soft[i] - in[i]), 2))/(2*(52-stop+start-1)); 
-			noise_non_interf += temp2;
+			noise_non_interf += (std::pow(std::abs(d_H_soft[i] - in[i]), 2)); 
 			}
-		}
-
-		// conventional noise variance estimate : when no interference, always do this
-		//{
 			conv_est += (std::pow(std::abs(d_H_soft[i] - in[i]), 2));
-		//}
-
+		
 			noise += std::pow(std::abs(d_H_soft[i] - in[i]), 2);
 			signal += std::pow(std::abs(d_H_soft[i] + in[i]), 2);
 			d_H_soft[i] += in[i]; // add d_H with in array i.e. second lts 
 			d_H_soft[i] /= LONG_soft[i] * gr_complex(2, 0); // channel estimation for current frame
 		}
-
-                if(d_interference){ // populate noise variance vector when interference
-		for(int i = 0; i < 64; i++)
-			{
-				if((i == 32) || (i < 6) || ( i > 58)) {continue;}
-				if(i > start && i < stop){d_N_soft_loc[i] = noise_interf;}
-				else { d_N_soft_loc[i] = noise_non_interf; }
-				//std::cout << " interference " << interference << "d_N_soft_loc[i] " << 					d_N_soft_loc[i] << std::endl; // to print local est
-			}
-		}
-                else{
-		for(int i = 0; i < 64; i++) // populate noise variance vector when no interference
-			{
-				if((i == 32) || (i < 6) || ( i > 58)) {continue;}
-				d_N_soft_conv[i] = conv_est/(2*52);
-				//std::cout << "interference " << interference << "d_N_soft_conv[i] " << 					d_N_soft_conv[i] << std::endl; // to print conv est
-			}
-		}
-
-
+		
 		d_snr_soft = 10 * std::log10(signal / noise / 2);
+                d_SIR = (float)(10 * std::log10((temp3/11)/(temp4/41)/ 2)); // SIR in some sense :) 
+		std::cout << "SIR " << (float)d_SIR << " "<< temp3/11 << " "<< temp4/41 <<std::endl;
+
+// assignment loop
+		for(int i = 0; i < 64; i++)
+		{
+			if((i == 32) || (i < 6) || ( i > 58)) 
+			{
+				continue;
+			}
+			if(i > start && i < stop)
+			{
+				d_N_soft_loc[i] = noise_interf/(2*(stop-start+1));
+			}
+			else 
+			{ 
+				d_N_soft_loc[i] = noise_non_interf/(2*(52-stop+start-1)); 
+			}
+
+			d_N_soft_conv[i] = conv_est/(2*52);
+		}
+	
+		if(d_SIR > d_threshold) {d_interference = 1;}
 
 	} else { // from n = 2 onwards, data symbols are there
 		if(d_interference && ((n - d_frame_symbols) == 2)){d_interference = 0; 
-                 //std::cout << "interference var reset, check again in new frame" << std::endl;
+                 std::cout << "interference var reset, check again in new frame" << std::endl;
 		}
-std::cout << "n->" << n <<" interference->" << d_interference << " d_frame_symbols->" << d_frame_symbols << std::endl;
+//std::cout << "n->" << n <<" interference->" << d_interference << " d_frame_symbols->" << d_frame_symbols << std::endl;
                 //std::cout << "n--" << n << "i-- " << imt << "d_frame_symbols" << d_frame_symbols << std::endl;
 		int c = 0;
 		for(int i = 0; i < 64; i++) {
--- lib/equalizer/ls_soft.h
+++ lib/equalizer/ls_soft.h
@@ -33,10 +33,11 @@ private:
 	gr_complex d_H_soft[64];
 	float d_N_soft_loc[64];
 	float d_N_soft_conv[64];
-	float d_N_soft_indv[64];
+	float d_threshold = 3;
 	double d_snr_soft;
 	int d_interference = 0; // interference present or not	
 	int imt = 0;
+	float d_SIR = 0;
 	
 };
 
