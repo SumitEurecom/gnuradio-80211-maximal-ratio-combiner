834bdcbb3cefa6089e1a6241663611df712f5f5b
Sumit <sumit.kumar@eurecom.fr>
2018-09-25 17:38:58 +0200

    sbmrc works for high snr only

---
+++ lib/sync_short_dc.cc
@@ -0,0 +1,176 @@
+/*
+ * Copyright (C) 2013, 2016 Bastian Bloessl <bloessl@ccs-labs.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <ieee802-11/sync_short_dc.h>
+#include <gnuradio/io_signature.h>
+#include "utils.h"
+
+#include <iostream>
+
+using namespace gr::ieee802_11;
+
+static const int MIN_GAP = 480;
+static const int MAX_SAMPLES = 540 * 80;
+static int ios1[] = {sizeof(gr_complex), sizeof(gr_complex),
+sizeof(gr_complex), sizeof(gr_complex), sizeof(float)};
+static std::vector<int> iosig1(ios1, ios1+sizeof(ios1)/sizeof(int));
+class sync_short_dc_impl : public sync_short_dc {
+
+public:
+sync_short_dc_impl(double threshold, unsigned int min_plateau, bool log, bool debug) :
+		block("sync_short_dc",
+			//gr::io_signature::makev(5, 5, get_input_sizes()),
+			gr::io_signature::makev(5, 5, iosig1),
+			gr::io_signature::make2(2, 2, sizeof(gr_complex), sizeof(gr_complex))),
+		d_log(log),
+		d_debug(debug),
+		d_state(SEARCH),
+		d_plateau(0),
+		d_freq_offset(0),
+		d_copied(0),
+		MIN_PLATEAU(min_plateau),
+		d_threshold(threshold) {
+
+	set_tag_propagation_policy(block::TPP_DONT);
+}
+
+int general_work (int noutput_items, gr_vector_int& ninput_items,
+		gr_vector_const_void_star& input_items,
+		gr_vector_void_star& output_items) {
+
+	const gr_complex *in = (const gr_complex*)input_items[0];
+	const gr_complex *in_1 = (const gr_complex*)input_items[1];
+	const gr_complex *in_abs = (const gr_complex*)input_items[2];
+	const gr_complex *in_abs_1 = (const gr_complex*)input_items[3];
+	const float *in_cor = (const float*)input_items[4];
+	gr_complex *out = (gr_complex*)output_items[0];
+	gr_complex *out_1 = (gr_complex*)output_items[1];
+
+int noutput = noutput_items;
+int ninput = std::min(std::min(ninput_items[2] ,std::min(ninput_items[0], ninput_items[1])), std::min(ninput_items[4], ninput_items[3]));
+	dout << "SHORT noutput : " << noutput << " ninput: " << ninput_items[0] << std::endl;
+
+	switch(d_state) {
+
+	case SEARCH: {
+		int i;
+
+		for(i = 0; i < ninput; i++) {
+			if(in_cor[i] > d_threshold) {
+				if(d_plateau < MIN_PLATEAU) {
+					d_plateau++;
+
+				} else {
+					d_state = COPY;
+					d_copied = 0;
+					d_freq_offset = arg(in_abs[i]) / 16;
+					d_freq_offset_1 = arg(in_abs_1[i]) / 16;
+					d_plateau = 0;
+		                        insert_tag(nitems_written(0), d_freq_offset, d_freq_offset_1, nitems_read(0) + i);
+					dout << "SHORT Frame! State Search" << std::endl;
+					break;
+				}
+			} else {
+				d_plateau = 0;
+			}
+		}
+
+		consume_each(i); // Tell the scheduler how_many_items were consumed on each input stream.
+
+
+		return 0;
+	}
+
+	case COPY: {
+
+		int o = 0;
+		while( o < ninput && o < noutput && d_copied < MAX_SAMPLES) {
+			if(in_cor[o] > d_threshold) {
+				if(d_plateau < MIN_PLATEAU) {
+					d_plateau++;
+
+				// there's another frame
+				} else if(d_copied > MIN_GAP) {
+					d_copied = 0;
+					d_plateau = 0;
+					d_freq_offset = arg(in_abs[o]) / 16;
+					d_freq_offset_1 = arg(in_abs_1[o]) / 16;
+					insert_tag(nitems_written(0) + o, d_freq_offset, d_freq_offset_1, nitems_read(0) + o);
+					dout << "SHORT Frame! State Copy" << std::endl;
+					break;
+				}
+
+			} else {
+				d_plateau = 0;
+			}
+
+			out[o] = in[o] * exp(gr_complex(0, -d_freq_offset * d_copied));
+			out_1[o] = in_1[o] * exp(gr_complex(0, -d_freq_offset_1 * d_copied));
+			o++;
+			d_copied++;
+		}
+
+		if(d_copied == MAX_SAMPLES) {
+			d_state = SEARCH;
+		}
+
+		dout << "SHORT copied EE" << o << std::endl;
+
+		consume_each(o);
+		return o;
+	}
+	}
+
+	throw std::runtime_error("sync short: unknown state");
+	return 0;
+}
+
+void insert_tag(uint64_t item, double freq_offset, double freq_offset_1, uint64_t input_item) {
+	mylog(boost::format("frame start at in: %2% out: %1%") % item % input_item);
+
+	const pmt::pmt_t key = pmt::string_to_symbol("wifi_start");
+	const pmt::pmt_t value = pmt::from_double(freq_offset);
+	const pmt::pmt_t value_1 = pmt::from_double(freq_offset_1);
+	const pmt::pmt_t srcid = pmt::string_to_symbol(name());
+	add_item_tag(0, item, key, value, srcid);
+	add_item_tag(1, item, key, value_1, srcid);
+}
+
+static std::vector<int> get_input_sizes(){
+    std::vector<int> input_sizes;
+    input_sizes.push_back(sizeof(gr_complex));
+    input_sizes.push_back(sizeof(gr_complex));
+    input_sizes.push_back(sizeof(gr_complex));
+    input_sizes.push_back(sizeof(gr_complex));
+    input_sizes.push_back(sizeof(float));
+
+    return input_sizes;
+    }
+private:
+	enum {SEARCH, COPY} d_state;
+	int d_copied;
+	int d_plateau;
+	float d_freq_offset, d_freq_offset_1;
+	const double d_threshold;
+	const bool d_log;
+	const bool d_debug;
+	const unsigned int MIN_PLATEAU;
+};
+
+sync_short_dc::sptr
+sync_short_dc::make(double threshold, unsigned int min_plateau, bool log, bool debug) {
+	return gnuradio::get_initial_sptr(new sync_short_dc_impl(threshold, min_plateau, log, debug));
+}
--- swig/ieee802_11_swig.i
+++ swig/ieee802_11_swig.i
@@ -29,6 +29,7 @@
 #include "ieee802-11/ether_encap.h"
 #include "ieee802-11/frame_equalizer.h"
 #include "ieee802-11/soft_frame_equalizer.h"
+#include "ieee802-11/soft_frame_equalizer_dc.h"
 #include "ieee802-11/mac.h"
 #include "ieee802-11/mapper.h"
 #include "ieee802-11/moving_average_cc.h"
@@ -36,7 +37,10 @@
 #include "ieee802-11/parse_mac.h"
 #include "ieee802-11/signal_field.h"
 #include "ieee802-11/sync_long.h"
+#include "ieee802-11/sync_long_dc.h"
 #include "ieee802-11/sync_short.h"
+#include "ieee802-11/sync_short_dc.h"
+#include "ieee802-11/sync_short_dc_sep_corr.h"
 %}
 
 %include "gnuradio/digital/packet_header_default.h"
@@ -53,6 +57,7 @@
 %include "ieee802-11/ether_encap.h"
 %include "ieee802-11/frame_equalizer.h"
 %include "ieee802-11/soft_frame_equalizer.h"
+%include "ieee802-11/soft_frame_equalizer_dc.h"
 %include "ieee802-11/mac.h"
 %include "ieee802-11/mapper.h"
 %include "ieee802-11/moving_average_cc.h"
@@ -60,7 +65,10 @@
 %include "ieee802-11/parse_mac.h"
 %include "ieee802-11/signal_field.h"
 %include "ieee802-11/sync_long.h"
+%include "ieee802-11/sync_long_dc.h"
 %include "ieee802-11/sync_short.h"
+%include "ieee802-11/sync_short_dc.h"
+%include "ieee802-11/sync_short_dc_sep_corr.h"
 
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, chunks_to_symbols);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, decode_mac);
@@ -68,13 +76,17 @@ GR_SWIG_BLOCK_MAGIC2(ieee802_11, decode_mac_soft);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, ether_encap);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, frame_equalizer);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, soft_frame_equalizer);
+GR_SWIG_BLOCK_MAGIC2(ieee802_11, soft_frame_equalizer_dc);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, mac);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, mapper);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, moving_average_cc);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, moving_average_ff);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, parse_mac);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, sync_long);
+GR_SWIG_BLOCK_MAGIC2(ieee802_11, sync_long_dc);
 GR_SWIG_BLOCK_MAGIC2(ieee802_11, sync_short);
+GR_SWIG_BLOCK_MAGIC2(ieee802_11, sync_short_dc);
+GR_SWIG_BLOCK_MAGIC2(ieee802_11, sync_short_dc_sep_corr);
 
 %template(signal_field_sptr) boost::shared_ptr<gr::ieee802_11::signal_field>;
 %pythoncode %{
